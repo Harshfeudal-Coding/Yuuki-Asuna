7c4f82902bea8e42e348d551786966f7f0a95e64
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2022-04-15 23:49:45 +0700

    sus

--- commands/ping.h
+++ commands/ping.h
@@ -2,17 +2,4 @@
 
 #include "../include.h"
 
-namespace ping_h
-{
-	void execute(const dpp::interaction_create_t& event, dpp::command_interaction command_data)
-	{
-		int ping = event.from->websocket_ping * 1000;
 
-		dpp::message mess;
-		mess.set_flags(dpp::m_ephemeral);
-		std::string str = "Ping: " + std::to_string(ping) + "ms";
-		mess.set_content(str);
-
-		event.reply(dpp::ir_channel_message_with_source, mess);
-	}
-}
