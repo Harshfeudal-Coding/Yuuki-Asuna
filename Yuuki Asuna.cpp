120a4ec57c218cf8284e439b8fe6614367d2c7d9
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2022-05-15 00:26:59 +0700

    Error: JSON throwing error to me :(

--- Yuuki Asuna.cpp
+++ Yuuki Asuna.cpp
@@ -18,15 +18,28 @@ int main()
 
 	// Constant
 	const auto &token = reader["token"];
-	const auto &guild_id = reader["guild_id"];
+	const dpp::snowflake &guild_id = reader["guild_id"];
 
 	// Token access
 	dpp::cluster client(token);
 
+	// Interaction create
+	client.on_interaction_create([&client](const dpp::interaction_create_t& event) {
+		if (event.command.get_command_name() == "ping") {
+			event.reply("Pong!");
+		}
+	});
+
 	// Bot ready
-	client.on_ready([&client](const dpp::ready_t& event) {
+	client.on_ready([&client, guild_id](const dpp::ready_t& event) {
 		fmt::print("Log in as {}.\n", client.me.format_username());
 		client.set_presence(dpp::presence(dpp::ps_dnd, dpp::at_watching, "Sword Art Online"));
+
+		// Guild command
+		client.guild_command_create(
+			dpp::slashcommand("ping", "Ping pong!", client.me.id),
+			guild_id
+		);
 	});
 	
 	// Starting the bot
---
+++ commands/ping.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Internal include
+#include <iostream>
+
+// External include
+#include <dpp/dpp.h>
+
+namespace ping_h
+{
+	void ping()
+	{
+		// Under-construction
+	}
+}
