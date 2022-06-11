0b663cb7989c406dba76ba982a4dd2f4786f9a22
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2022-06-11 14:43:00 +0700

    formatting slashcommand (still not work in somehow)

--- src/main.cpp
+++ src/main.cpp
@@ -33,35 +33,39 @@ int main()
 	client.on_ready([&client, &reader](const dpp::ready_t& event) {
 		fmt::print("Log in as {}.\n", client.me.format_username());
 		client.set_presence(dpp::presence(dpp::ps_dnd, dpp::at_watching, "Sword Art Online"));
+		});
+
+	client.on_slashcommand([&client, &reader](const dpp::slashcommand_t& event) {
 
 		// Make slash commands
 		std::vector<dpp::slashcommand> commands
 		{
 			dpp::slashcommand()
 				.set_name("Ping")
-				.set_description("Testing ping command")
+				.set_description("Test ping command")
 				.set_application_id(client.me.id),
 			dpp::slashcommand()
 				.set_name("Hi")
 				.set_description("Only say hi")
 				.set_application_id(client.me.id),
-
-			/* Please note:
-				- The slash commands are under-construction!
-				- If you want to use it in Visual Studio, make a new file called `config.json`
-				- Place your token and guild ID in there, at the same directory of `.gitignore`
-				
-				Thanks!
-			*/
 		};
 
 		// Guild slash command create
 		client.guild_bulk_command_create(
-			commands, 
-			reader["guild_id"].get<dpp::snowflake>()
+			commands,
+			reader["guild_id"].get<dpp::snowflake>(),
+			dpp::utility::log_error()
 		);
 
-		// End of the on_ready
+		/*
+			Please note:
+				- The slash commands are under-construction!
+				- If you want to use it in Visual Studio, make a new file called `config.json`
+				- Place your token and guild ID in there, at the same directory of `.gitignore`
+				- The on_slashcommand in somehow it still doesn't work in the guild, please wait until I solve it.
+
+			Thanks!
+		*/
 		});
 
 	// Bot start
