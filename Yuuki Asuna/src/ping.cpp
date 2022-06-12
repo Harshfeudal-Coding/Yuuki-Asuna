525e0edcac22de9c02871f1b25d83c377d22c20f
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2022-06-12 22:58:47 +0700

    still not reply :(

--- Yuuki Asuna/src/ping.cpp
+++ Yuuki Asuna/src/ping.cpp
@@ -1,4 +1,3 @@
-
 // Project include
 #include "../commands/handler.h"
 #include "../commands/ping.h"
@@ -13,10 +12,18 @@ void ping(dpp::cluster& client, const dpp::slashcommand_t& event)
 
 	// Setup message reply
 	dpp::message message;
-	std::string message_content = fmt::format("Asuna measured: {0:.02f} ms", (client.rest_ping + ws_ping) * 1000);
+	std::string message_content = fmt::format(
+		"Asuna measured: {0:.02f} ms", 
+		(client.rest_ping + ws_ping) * 1000
+	);
 
 	// Message output ready
 	message.set_flags(dpp::m_ephemeral);
 	message.set_content(message_content);
-	event.reply(dpp::ir_channel_message_with_source, message_content);
+
+	// Message reply
+	event.reply(
+		dpp::ir_channel_message_with_source, 
+		message
+	);
 }
